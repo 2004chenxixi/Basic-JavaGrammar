b8c5b9f93a2119ba395ee58f6e0c4ee5460e35bc
chenxixi <@111111.com>
2022-05-19 21:26:37 +0800

    基本数据类型<-->包装类

--- src/BasicDataTypeChangePackage/basicDataTypeChangePackage.java
+++ src/BasicDataTypeChangePackage/basicDataTypeChangePackage.java
@@ -11,6 +11,7 @@ public class basicDataTypeChangePackage {
         b.WrapperTest();
         b.WrapperTest1();
     }
+
     //1--这个是基本数据类型---->包装类
     //用Integer.valueOf()  来转化
     public void WrapperTest() {
@@ -27,10 +28,22 @@ public class basicDataTypeChangePackage {
     public void WrapperTest1() {
         Integer num1 = Integer.valueOf(12);
         int i = num1.intValue();
-        System.out.println(i+1);
-
+        System.out.println(i + 1);
     }
 
+    public void WrapperTestTotal() {
+        //上面的：基本数据类型<-->包装类,太麻烦，jdk给了我们（自动装箱和自动拆箱）
+        int I1 = 10;
+        Integer ChangeI1 = I1; //*这里的int-I1  可以直接变成  Integer-ChangeI1
+
+        Integer I2 = 32;
+        int ChangeI2 = I2;  //*这里的Integer-ChangeI1  可以直接变成  int-I2
+
+        /*
+        对于-基本数据类型<-->包装类-直接使用就可以
+         */
+
+    }
 
 
 }
