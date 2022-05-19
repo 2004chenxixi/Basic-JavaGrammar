5e5c910a2e67efc38c259a9b3226e7545b5905db
chenxixi <@111111.com>
2022-05-19 21:48:06 +0800

    String <--> 基本数据类型、包装类

--- src/BasicDataTypeChangePackage/basicDataTypeChangePackage.java
+++ src/BasicDataTypeChangePackage/basicDataTypeChangePackage.java
@@ -1,7 +1,7 @@
 package BasicDataTypeChangePackage;
 
 /*
-基本数据类型-->包装类
+(基本数据类型<-->包装类) <---> String  [三种类型的转化]
 在包装类中，大家都变成了类
  */
 
@@ -10,6 +10,7 @@ public class basicDataTypeChangePackage {
         basicDataTypeChangePackage b = new basicDataTypeChangePackage();
         b.WrapperTest();
         b.WrapperTest1();
+        b.WrapperTestChangeString();
     }
 
     //1--这个是基本数据类型---->包装类
@@ -38,12 +39,35 @@ public class basicDataTypeChangePackage {
 
         Integer I2 = 32;
         int ChangeI2 = I2;  //*这里的Integer-ChangeI1  可以直接变成  int-I2
-
-        /*
-        对于-基本数据类型<-->包装类-直接使用就可以
-         */
-
     }
+        /*
+        One--对于-基本数据类型<-->包装类-直接使用就可以
+         */
+//——————————————————————————————————————————————————————————————————————————————————————————
+        /*
+       Two --(基本数据类型<-->包装类) <---> String
+         */
+   public void  WrapperTestChangeString() {
+       //基本数据类型-->String
+       //方法1  ---> xxx + ""  = String类型
+       int a = 10;
+       String A = a + "1";
+       System.out.println(A);
+
+       //方法2 ---> StringValueOf()
+       int b = 10;
+       String B = String.valueOf(b);
+       System.out.println(B);
+
+       //String--->基本数据类、包装类
+       //使用  包装类.parse
+       String C ="123";
+       Integer c =  Integer.parseInt(C);
+       System.out.println(c);
+   }
+
+
+
 
 
 }
