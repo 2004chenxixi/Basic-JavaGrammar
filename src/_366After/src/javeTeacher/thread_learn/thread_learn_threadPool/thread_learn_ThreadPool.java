5d7e9661ccfb07403c92ec9cf084c12124221aed
chenxixi <@111111.com>
2022-05-31 22:45:29 +0800

    线程（thread）--446（尚硅谷） 线程池

--- src/_366After/src/javeTeacher/thread_learn/thread_learn_threadPool/thread_learn_ThreadPool.java
+++ src/_366After/src/javeTeacher/thread_learn/thread_learn_threadPool/thread_learn_ThreadPool.java
@@ -4,5 +4,17 @@ package _366After.src.javeTeacher.thread_learn.thread_learn_threadPool;
 使用--线程池
  */
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class thread_learn_ThreadPool {
+    public static void main(String[] args) {
+        //设置-线程池的最大容量
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        service.execute();//适合-适用于Runnable
+        service.submit();//适合-适用于Callable
+
+
+    }
 }
