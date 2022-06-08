d62221ea575cd58448035d296253287bc8f865a5
chenxixi <@111111.com>
2022-06-08 16:52:00 +0800

    519 -集合常用的方法

--- src/_366After/src/javeTeacher/gather/learn_gather/Gather.java
+++ src/_366After/src/javeTeacher/gather/learn_gather/Gather.java
@@ -2,11 +2,11 @@ package _366After.src.javeTeacher.gather.learn_gather;
 
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
+import java.util.*;
 
 //Collection接口中的使用方法
+//总结：
+//*当我们要添加Obj时，要求obj所在的类要----重写equal方法
 public class Gather {
     @Test
     public void test1() {
@@ -58,14 +58,89 @@ public class Gather {
         collection.add("aa");
         collection.add("BB");
         collection.add(123);//自动装箱子
+        collection.add(45);//自动装箱子
+        collection.add(67);//自动装箱子
         collection.add(new Date());
         collection.add(new person("张三", 20));
         collection.add(new person("李四", 25));
 
-        //8- 移除数据 --放回值为Boolen  --->true为移除成功/false
+        //8- remove 移除数据 --放回值为Boolen  --->true为移除成功/false
         boolean remove = collection.remove(123);
-        System.out.println("内部信息= " + collection);
         System.out.println("查看是否移除成功= " + remove);
         System.out.println("内部信息-被移除后 = " + collection);
+
+        //9- removeAll  -->以Coll1的内容为模版，在 Collection 删除与coll1相同内容的部分，当时coll1不受影响
+        //模版删除
+        Collection coll1 = Arrays.asList(45, 67);
+        boolean rmAll = collection.removeAll(coll1);
+        System.out.println("查看模版删除--是否成功= " + rmAll);
+        System.out.println("coll1为模版的内容= " + coll1);
+        System.out.println("以Coll1的内容为模版，在 Collection 删除与coll1相同内容的部分= " + collection);
+    }
+
+    @Test
+    public void test3() {
+        Collection collection = new ArrayList();
+        collection.add("aa");
+        collection.add("BB");
+        collection.add(123);//自动装箱子
+        collection.add(45);//自动装箱子
+        collection.add(67);//自动装箱子
+        collection.add(new Date());
+        collection.add(new person("张三", 20));
+        collection.add(new person("李四", 25));
+
+        //10- 模版查询 -retainAll  --->  Collection 以 Coll1为模版，输出和Coll1（共有的内容）
+        Collection coll1 = Arrays.asList(45, 67);
+        collection.retainAll(coll1);
+        System.out.println(collection);
+    }
+
+    @Test
+    public void test4() {
+        Collection collection = new ArrayList();
+        collection.add("aa");
+        collection.add("BB");
+        collection.add(123);//自动装箱子
+        collection.add(45);//自动装箱子
+        collection.add(67);//自动装箱子
+        //collection.add(new Date());
+        collection.add(new person("张三", 20));
+        collection.add(new person("李四", 25));
+
+        Collection collection2 = new ArrayList();
+        collection2.add("aa");
+        collection2.add("BB");
+        collection2.add(123);//自动装箱子
+        collection2.add(45);//自动装箱子
+        collection2.add(67);//自动装箱子
+        //collection.add(new Date());
+        collection2.add(new person("张三", 20));
+        collection2.add(new person("李四", 25));
+
+        //11-equal  比较两个集合的内容-是否相等
+        System.out.println("比较两个集合的内容-是否相等=  " + collection.equals(collection2));
+
+        //12- hashCode 哈希值
+        System.out.println("collection的哈希值= " + collection.hashCode());
+
+        System.out.println("-----------------------------------------");
+
+        //13-toArray  集合 --> 数组
+        Object[] arr = collection.toArray();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("我是 集合 --> 数组 便利效果=  " + arr[i]);
+        }
+
+        System.out.println("-----------------------------------------");
+
+        //14-asList 数组 --> 集合
+        List<String> list = Arrays.asList(new String[]{"aa", "bb", "cc"});
+        System.out.println("数组 --> 集合= " + list);
+        //数组(要用包装类) --> 集合 ---的细节
+        List<Integer> arr2 = Arrays.asList(new Integer[]{133, 435});
+        System.out.println("数组 --> 集合（要用包装类）= " + arr2);
+
+        //15- iterator();放回Iterator接口的实例，用于便利集合元素---在iteratorTest中
     }
 }
