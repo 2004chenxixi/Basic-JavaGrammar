c44b0a5a92d1425831882ca17282e8e0c996e46f
chenxixi <@111111.com>
2022-06-14 11:00:12 +0800

    591 --字节流（复制照片）

--- src/_366After/src/javeTeacher/IO/file_inPut_outPut/File_InPut_OutPut.java
+++ src/_366After/src/javeTeacher/IO/file_inPut_outPut/File_InPut_OutPut.java
@@ -24,7 +24,7 @@ public class File_InPut_OutPut {
             fileInputStream = new FileInputStream(srcFile);
             fileOutputStream = new FileOutputStream(destFile);
             //3- 复制操作
-            byte[] buffer = new byte[5];
+            byte[] buffer = new byte[1024];
             int len;
             while ((len = fileInputStream.read(buffer)) != -1) {
                 fileOutputStream.write(buffer, 0, len);
@@ -50,7 +50,56 @@ public class File_InPut_OutPut {
                 }
 
         }
+    }
 
+    //指定路径下的文件复制(----- 方法的封装)
+    public void copyFile(String srcPath, String destPath) {
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            //1- 读，写的两个文件-实例化
+            File srcFile = new File(srcPath);
+            File destFile = new File(destPath);
+
+            //2- 给读取，写出--一个流
+            fileInputStream = new FileInputStream(srcFile);
+            fileOutputStream = new FileOutputStream(destFile);
+            //3- 复制操作
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, len);
+            }
+            System.out.println("复制成功---Success");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //4- 关闭流
+
+            if (fileInputStream != null)
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            if (fileOutputStream != null)
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+        }
+    }
+
+    @Test
+    public void Copy() {
+        long start = System.currentTimeMillis();
+        copyFile("方园.jpeg", "方园2.jpeg");
+        long end = System.currentTimeMillis();
+        //执行了多少秒
+        System.out.println("复制一份文件，执行了 = " + (end - start) + " 秒");
 
     }
 }
