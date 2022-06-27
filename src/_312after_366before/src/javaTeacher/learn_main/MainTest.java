4ca987f4d4206f4819b366cdcfb999246a91238a
chenxixi <@111111.com>
2022-06-27 14:38:49 +0800

    328-Main的理解

---
+++ src/_312after_366before/src/javaTeacher/learn_main/MainTest.java
@@ -0,0 +1,15 @@
+package _312after_366before.src.javaTeacher.learn_main;
+
+/*
+main()方法的说明
+1- main（）方法作为程序的入口
+---->  public static void main(String[] args) {}一个入口
+
+2- main（）也是一个普通的静态方法
+
+3- main（）方法可以作为我们于控制台的交互的方式（）
+ */
+public class MainTest {
+    public static void main(String[] args) {
+    }
+}
